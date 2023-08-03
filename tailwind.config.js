6aabf54c531eca6963eaca0a94d8c0619999d5ed
TVV <TVV@d.c>
2023-08-03 23:54:56 +0300

    header desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./public/**/*.html"],
   theme: {
     extend: {},
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      sand: "#fcf7e6",
+    },
+    fontFamily: {
+      main: ["Space Grotesk"],
+    },
   },
   plugins: [],
 };
